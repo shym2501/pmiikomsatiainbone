57373b9b33e05f0191bb9bb0b1c26f7e0bbf7eee
Shym2501 <sulthannurya@gmail.com>
2022-08-28 23:05:40 +0700

    update

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -6,9 +6,9 @@
 					<div class="row">
 						<div class="col-8">
 							<div class="numbers">
-								<p class="text-sm mb-0 text-uppercase font-weight-bold">Today's Money</p>
+								<p class="text-sm mb-0 text-uppercase font-weight-bold">Jumlah Kader</p>
 								<h5 class="font-weight-bolder">
-									$53,000
+									10.000
 								</h5>
 								<p class="mb-0">
 									<span class="text-success text-sm font-weight-bolder">+55%</span>
