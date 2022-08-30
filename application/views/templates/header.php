e574dc846e299aa5424f811615396ac5623201b6
Shym2501 <sulthannurya@gmail.com>
2022-08-30 11:27:28 +0700

    update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -17,11 +17,11 @@
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/'); ?>css/nucleo-icons.css" rel="stylesheet">
     <link href="<?= base_url('assets/'); ?>css/nucleo-svg.css" rel="stylesheet">
-    <link href="<?= base_url('assets/'); ?>css/argon-dashboard.css?v=2.0.4" id="pagestyle" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>css/argon-dashboard.css" id="pagestyle" rel="stylesheet">
     <link href="<?= base_url('assets/'); ?>css/style.css" rel="stylesheet">
 
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
 </head>
-<body class="g-sidenav-show   bg-gray-100">
+<body class="g-sidenav-show   bg-gray-200">
     <div class="min-height-300 bg-primary position-absolute w-100"></div>
