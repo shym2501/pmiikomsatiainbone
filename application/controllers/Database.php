cab98ab973b0355da3a8783540b94146da35a235
Shym2501 <sulthannurya@gmail.com>
2022-09-06 15:07:43 +0700

    update

--- application/controllers/Database.php
+++ application/controllers/Database.php
@@ -18,4 +18,15 @@ class Database extends CI_Controller
         $this->load->view('database/index', $data);
         $this->load->view('templates/footer');
     }
+
+    public function detail_kader()
+    {
+        $data['title'] = 'Detail Kader';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/navbar', $data);
+        $this->load->view('database/detail_kader', $data);
+        $this->load->view('templates/footer');
+    }
 }
---
+++ application/views/database/detail_kader.php
@@ -0,0 +1,145 @@
+<div class="card card-profile">
+    <img src="../assets/img/bg-profile.jpg" alt="Image placeholder" class="card-img-top">
+    <div class="row justify-content-center">
+        <div class="col-4 col-lg-4 order-lg-2">
+            <div class="mt-n4 mt-lg-n6 mb-4 mb-lg-0">
+                <a href="javascript:;">
+                    <img src="../assets/img/team-2.jpg" class="rounded-circle img-fluid border border-2 border-white">
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="card-header text-center border-0 pt-0 pt-lg-2 pb-4 pb-lg-3">
+        <div class="d-flex justify-content-between">
+            <a href="javascript:;" class="btn btn-sm btn-dark mb-0 d-none d-lg-block">Blacklist</a>
+            <a href="javascript:;" class="btn btn-sm btn-dark mb-0 d-block d-lg-none"><i class="ni ni-collection"></i></a>
+            <a href="javascript:;" class="btn btn-sm btn-danger float-right mb-0 d-none d-lg-block">Delete</a>
+            <a href="javascript:;" class="btn btn-sm btn-danger float-right mb-0 d-block d-lg-none"><i class="ni ni-email-83"></i></a>
+        </div>
+    </div>
+    <div class="card-body pt-0">
+        <div class="row">
+            <div class="col">
+                <div class="d-flex justify-content-center">
+                    <div class="d-grid text-center">
+                        <span class="text-lg font-weight-bolder">22</span>
+                        <span class="text-sm opacity-8">Friends</span>
+                    </div>
+                    <div class="d-grid text-center mx-4">
+                        <span class="text-lg font-weight-bolder">10</span>
+                        <span class="text-sm opacity-8">Photos</span>
+                    </div>
+                    <div class="d-grid text-center">
+                        <span class="text-lg font-weight-bolder">89</span>
+                        <span class="text-sm opacity-8">Comments</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="text-center mt-4">
+            <h5>
+                Mark Davis<span class="font-weight-light">, 35</span>
+            </h5>
+            <div class="h6 font-weight-300">
+                <i class="ni location_pin mr-2"></i>Bucharest, Romania
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header pb-0">
+        <div class="d-flex align-items-center">
+            <p class="text-uppercase text-sm mb-0">Edit Profile</p>
+            <button class="btn btn-primary btn-sm ms-auto">Edit & Save</button>
+        </div>
+    </div>
+    <div class="card-body">
+        <p class="text-sm">Biodata</p>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Nama Lengkap</label>
+                    <input class="form-control" type="text" value="luckyjesse">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Nama Panggilan</label>
+                    <input class="form-control" type="text" value="Jesse">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Alamat</label>
+                    <input class="form-control" type="text" value="Barebbo">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Tempat Lahir</label>
+                    <input class="form-control" type="text" value="Parippung">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Tanggal Lahir</label>
+                    <input class="form-control" type="date" value="Lucky">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Fakultas</label>
+                    <input class="form-control" type="text" value="Tarbiyah">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Jurusan</label>
+                    <input class="form-control" type="text" value="TBI">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Email</label>
+                    <input class="form-control" type="email" value="shym@gmail.com">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">No.HP/WA</label>
+                    <input class="form-control" type="text" value="085255xxxxxx">
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Foto</label>
+                    <input class="form-control" type="file">
+                </div>
+            </div>
+        </div>
+
+        <hr class="horizontal dark">
+        <p class="text-sm">Informasi Tambahan</p>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">Pelatihan</label>
+                    <input class="form-control" type="textarea" value="A beautiful Dashboard for Bootstrap 5. It is Free and Open Source.">
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group">
+                    <label for="example-text-input" class="form-control-label">About me</label>
+                    <input class="form-control" type="text area" value="A beautiful Dashboard for Bootstrap 5. It is Free and Open Source.">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
